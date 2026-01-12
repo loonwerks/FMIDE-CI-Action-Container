ea90fc68b15a5a6b28dae9092388cd16bafd1ab7
Karl Hoech <karl.hoech@collins.com>
2026-01-12 10:39:10 -0600

    Remove BreifCASE; not part of INSPECTA for now

--- make_fmide.sh
+++ make_fmide.sh
@@ -27,5 +27,5 @@ popd
 
 ${FMIDE_DIR}/osate -nosplash -console -consoleLog -application org.eclipse.equinox.p2.director -repository ${RESOLUTE_UPDATE_SITE} -installIU ${RESOLUTE_FEATURE_ID}/${RESOLUTE_VERSION}
 ${FMIDE_DIR}/osate -nosplash -console -consoleLog -application org.eclipse.equinox.p2.director -repository ${AGREE_UPDATE_SITE} -installIU ${AGREE_FEATURE_ID}/${AGREE_VERSION}
-${FMIDE_DIR}/osate -nosplash -console -consoleLog -application org.eclipse.equinox.p2.director -repository ${BRIEFCASE_UPDATE_SITE} -installIU ${BRIEFCASE_FEATURE_ID}/${BRIEFCASE_VERSION}
+# ${FMIDE_DIR}/osate -nosplash -console -consoleLog -application org.eclipse.equinox.p2.director -repository ${BRIEFCASE_UPDATE_SITE} -installIU ${BRIEFCASE_FEATURE_ID}/${BRIEFCASE_VERSION}
 ${FMIDE_DIR}/osate -nosplash -console -consoleLog -application org.eclipse.equinox.p2.director -repository ${HAMR_UPDATE_SITE} -installIU ${HAMR_FEATURE_ID}/${HAMR_VERSION}
